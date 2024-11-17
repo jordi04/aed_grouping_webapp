8acdb19acd720649790fdeb92d036371e5f33ca0
Jaume Mora <moraladariajaume@gmail.com>
2024-11-17 12:40:28 +0100

    minor changes

--- App/jsonDownloader.py
+++ App/jsonDownloader.py
@@ -19,14 +19,18 @@ participants = participants_ref.stream()
 
 # Create a list to hold participant data
 participants_data = []
+seen_ids = set()
 
 # Iterate through the documents and add them to the list
 for participant in participants:
-    participants_data.append(participant.to_dict())
+    participant_dict = participant.to_dict()
+    participant_id = participant_dict.get("id")
+    if participant_id not in seen_ids:
+        participants_data.append(participant_dict)
+        seen_ids.add(participant_id)
 
 # Write the data to a local JSON file
 with open("datathon_participants.json", "w") as file:
     json.dump(participants_data, file, indent=4)
 
 print("Firestore data downloaded to JSON file successfully!")
-
