30508ada5521a6d918800a489a9f3a3d9229f7ca
Jaume Mora <moraladariajaume@gmail.com>
2024-11-17 12:29:16 +0100

    Push final

--- App/grouping.py
+++ App/grouping.py
@@ -52,38 +52,6 @@ def load_participants(path: str) -> List[Participant]:
 
 
 # Load data
-# Cridem que s'executi jsonDownloader.py per obtenir el fitxer datathon_participants.json
-
-
-# Initialize Firebase
-cred = credentials.Certificate("firebaseCred.json")
-firebase_admin.initialize_app(cred)
-
-# Initialize Firestore client
-db = firestore.client()
-
-# Reference to the collection where participants are stored
-participants_ref = db.collection("participants")
-
-# Get all documents in the collection
-participants = participants_ref.stream()
-
-# Create a list to hold participant data
-participants_data = []
-
-# Iterate through the documents and add them to the list
-for participant in participants:
-    participants_data.append(participant.to_dict())
-
-# Write the data to a local JSON file
-with open("datathon_participants.json", "w") as file:
-    json.dump(participants_data, file, indent=4)
-
-print("Firestore data downloaded to JSON file successfully!")
-
-
-
-
 data_path = "datathon_participants.json"
 participants = load_participants(data_path)
 
